82f6d5fc60bb5bbb999c391637632a258fbf0820
MukendiJonabas <jonabasmukendi02@gmail.com>
2024-11-19 11:58:58 +0100

    création header et des autres sections

--- components/Confiance.tsx
+++ components/Confiance.tsx
@@ -1,20 +1,20 @@
+import Image from "next/image";
+import Banner from "@/public/Banner.jpg"
 export default function Confiance(){
     return(
         <section>
         <div className="mx-auto px-5 pt-32 pb-24 lg:px-24">
           <div className="my-3 flex w-full flex-col text-left lg:text-center">
             <h2 className="bold mb-8 text-4xl font-bold leading-tight text-black lg:text-6xl">
-              Lorem ipsum elit sit unar,{" "}
+            Expertise 360°{" "}
               <br className="hidden lg:inline-block" />
-              consectetur adipiscing elit.
+              
             </h2>
           </div>
         <div className="flex w-full flex-col text-left lg:text-center">
         <h3 className="text-2xl text-black">
-          Lorem ipsum arcu, consectetur adipiscing elit. Viverra elementum
-          pellentesque <br className="hidden lg:inline-block" />
-          tortor, luctus blandit sed dolor et, semper. Posuere vitae vitae,
-          ac mus. Arcu quis feugiat.
+        Nous simplifions la complexité pour vous aider à innover. <br className="hidden lg:inline-block" />
+        De l’audit à l’implémentation, nous sommes à vos côtés à chaque étape. 
         </h3>
       </div>
       <div className="flex w-full flex-row justify-center pt-24 text-center">
@@ -33,7 +33,7 @@ export default function Confiance(){
       </div>
 
     <div className="invisible mx-auto flex max-w-6xl p-3 pb-32 lg:visible lg:px-2">
-      <img src="/images/placeholder.png" alt="img" />
+      <Image src={Banner} alt="img" />
     </div>
     </div>
     </section>
