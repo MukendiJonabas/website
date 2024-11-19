08c3d9dc285c8c26283f921a3c0318597aa6e022
JEAN KAZADI <jk08250825gmail.com>
2024-11-19 15:21:10 +0100

    modif de la sect/footer, paayday, Herosect.. par l'ajout des elements txt et img

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,158 +1,68 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 
 export default function Footer() {
-    return (
-      <>
-        <footer className="grotesk bg-[#f9fbfb]">
-          <div className="max-w-8xl mx-auto px-5 py-24 text-black">
-            <div className="order-first flex flex-wrap text-left">
-              <div className="w-full px-4 md:w-2/4 lg:w-1/5">
-                <h2 className="mb-3 text-lg tracking-widest">Est.</h2>
-                <nav className="list-none space-y-2 py-3">
-                  <li>
-                    <a href="/">Vitae nec.</a>
-                  </li>
-                  <li>
-                    <a href="/">Purus</a>
-                  </li>
-                  <li>
-                    <a href="/">Nibh.</a>
-                  </li>
-                  <li>
-                    <a href="/">Proin semper justo.</a>
-                  </li>
-                  <li>
-                    <a href="/">Blandit.</a>
-                  </li>
-                  <li>
-                    <a href="/">Malesuada.</a>
-                  </li>
-                </nav>
-              </div>
-              <div className="w-full px-4 md:w-2/4 lg:w-1/5">
-                <h2 className="mb-3 text-lg tracking-widest">Et.</h2>
-                <nav className="mb-10 list-none space-y-2 py-3">
-                  <li>
-                    <a href="/">Ninc elementum.</a>
-                  </li>
-                  <li>
-                    <a href="/">Sit ac interdum</a>
-                  </li>
-                  <li>
-                    <a href="/">Ac ut cras.</a>
-                  </li>
-                  <li>
-                    <a href="/">Sed ipsum lobortis.</a>
-                  </li>
-                  <li>
-                    <a href="/">Nulla maecenas nunc.</a>
-                  </li>
-                  <li>
-                    <a href="/">Purus</a>
-                  </li>
-                </nav>
-              </div>
-              <div className="w-full px-4 md:w-2/4 lg:w-1/5">
-                <h2 className="mb-3 text-lg tracking-widest">Placerat.</h2>
-                <nav className="mb-10 list-none space-y-2 py-3">
-                  <li>
-                    <a href="/">Et cursus fringilla.</a>
-                  </li>
-                  <li>
-                    <a href="/">In velit sagittis.</a>
-                  </li>
-                  <li>
-                    <a href="/">Mattis.</a>
-                  </li>
-                  <li>
-                    <a href="/">Est.</a>
-                  </li>
-                </nav>
-              </div>
-              <div className="w-full px-4 md:w-2/4 lg:w-1/5">
-                <h2 className="mb-3 text-lg tracking-widest">Messa.</h2>
-                <nav className="mb-10 list-none space-y-2 py-3">
-                  <li>
-                    <a href="/">Id.</a>
-                  </li>
-                  <li>
-                    <a href="/">Aliquam.</a>
-                  </li>
-                  <li>
-                    <a href="/">Interdum.</a>
-                  </li>
-                  <li>
-                    <a href="/">Risus.</a>
-                  </li>
-                </nav>
-              </div>
-              <div className="w-full md:w-2/4 lg:w-1/5">
-                <a href="/">
-                  <div className="relative border border-black transition hover:border-gray-500">
-                    <div className="absolute top-0 right-0 pt-2 pr-2">
-                      <svg
-                        width="8"
-                        height="8"
-                        viewBox="0 0 8 8"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M6.66992 0.747559L0.669922 6.74756"
-                          stroke="black"
-                        />
-                        <path
-                          d="M0.669922 0.747559H6.66992V6.74756"
-                          stroke="black"
-                        />
-                      </svg>
-                    </div>
-                    <div className="p-6">
-                      Lorem ipsum accumsan arcu, consectetur adipiscing elit.
-                      Consequat arcu.
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
+  return (
+    <footer className=" bg-zinc-600 text-gray-50 font-light">
+      <div className="flex flex-col md:flex-row gap-1">
+        <div className="p-5 flex flex-col md:flex-row w-full  ">
+          <div className=" basis-3/12">
+            <p className="font-bold underline">Nos Produits</p>
+            <ol className="">
+              <li>Easypay</li>
+              <li>RollApp</li>
+              <li>PayDay</li>
+              <li>Digipublic</li>
+              <li>..</li>
+            </ol>
           </div>
-          <div className="px-2">
-            <div className="max-w-8xl mx-auto px-5 py-6">
-              <h2 className="text-black">Diam egestas ultrices odio vitae.</h2>
-              <div>
-                <h2 className="my-4 text-sm">
-                  Lorem ipsum accumsan arcu, consectetur adipiscing elit. Dolor
-                  proin tempor sed fermentum sit{" "}
-                  <br className="hidden lg:inline-block" /> pretium pellentesque.
-                  Dictumst risus elementum dignissim risus, lobortis molestie.
-                </h2>
-              </div>
-              <div className="absolute right-0 -mt-24 hidden text-black lg:inline-block">
-                <a href="/" className="mr-16">
-                  Terms & Conditions
-                </a>
-                <a href="/" className="mr-16">
-                  Privacy Policy
-                </a>
-                <a href="/" className="mr-16">
-                  Cookie Policy
-                </a>
-              </div>
-              <div className="right-0 inline-block pt-12 pb-6 pr-20 text-sm text-black md:hidden">
-                <a href="/" className="mr-16">
-                  Terms & Conditions
-                </a>
-                <a href="/" className="mr-16">
-                  Privacy Policy
-                </a>
-                <a href="/" className="mr-16">
-                  Cookie Policy
-                </a>
-              </div>
-            </div>
+          <div className=" basis-3/12">
+            <p className="font-bold underline">Nos address & contacts</p>
+            <ol className="">
+              <li>
+                68, Avenue Batetela, Commune de la Gombe, Kinshasa, R.D. Congo.
+              </li>
+              <li>+243 81 333 87 77</li>
+              <li>+243 999 90 30 30</li>
+              <li>info@biosyc.com</li>
+              <li>admin@biosyc.com</li>
+            </ol>
           </div>
-        </footer>
-      </>
-    );
-  }
-  
+          <div className="  basis-3/12">
+            <p className="font-bold underline">Nos services</p>
+            <ol className="">
+              <li>Management des projets</li>
+              <li>Intégration(standarisation des systèmes)</li>
+              <li>Turnkey Delivery</li>
+
+              <li>Etc..</li>
+            </ol>
+          </div>
+          <div className=" basis-3/12">
+            <p className="font-bold underline">Specialités en solution</p>
+            <ol className="">
+              <li>Recensement et Identification National</li>
+              <li>Production des license de conduction electronic</li>
+              <li>Production des passports Biometrics</li>
+              <li>Production des visas et Système des visa</li>
+              <li>EDMS(Electronic Documents Management System)</li>
+              <li>Electronic Archiving System (Eas)</li>
+              <li>Smart Justice System(SJS)</li>
+              <li>Solution Electronic de Payment (EPS)</li>
+              <li>Digital Migration</li>
+              <li>Aerial Photo and Mapping</li>
+              <li>Energie Renouvelable</li>
+            </ol>
+            <a href="https://www.biosyc.com/specialised-solutions/">
+              <button className="text-sm p-1 border-2 border-teal-200 bg-teal-200 text-teal-900 font-bold rounded hover:bg-teal-800 hover:text-teal-50 hover:border-teal-800">
+                Learn More..
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className=" border-t-2 border-gray-100 text-gray-100 w-full p-3">
+        &copy; Copyright 2024 Sycamore S.A.R.L All Rights Reserved
+      </div>
+    </footer>
+  );
+}
